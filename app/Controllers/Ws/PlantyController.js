662ece065cb212e3a0141a8085d0e7d1b57d83dd
Isaac A. Marin <VasMar_ia@hotmail.com>
2019-03-26 12:20:54 -0600

    run sudo

--- app/Controllers/Ws/PlantyController.js
+++ app/Controllers/Ws/PlantyController.js
@@ -1,14 +1,19 @@
 'use strict'
 
+var sudo = require('sudo-js');
+sudo.setPassword('vasquez123');
+
 class PlantyController {
   constructor ({ socket, request }) {
     this.socket = socket
     this.request = request
   }
+
   onRiego(data){
     console.log(data)
-    this.socket.emit('riego',{response:'Success'})
+    this.socket.emit('riegos','hesll')
   }
+  
 }
 
 module.exports = PlantyController
